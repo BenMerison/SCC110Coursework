5e60b049f7f854f6831486fb58ddfea6f00d7cb4
BenMerison <b.merison@lancaster.ac.uk>
2020-03-16 18:12:47 +0000

    Completed board layout and added action listener. Buttons do not work correctly when clicked

--- Board.java
+++ Board.java
@@ -1,27 +1,109 @@
 import javax.swing.*;
 import java.awt.*;
 import java.applet.Applet;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Board 
+public class Board implements ActionListener
 {
-    public static void main (String[] args)
+    private Square buttons[][] = new Square[5][5];
+
+    public Board ()
     {
-        JFrame a = new JFrame();
-        JPanel gridPanel = new JPanel();
-                              
-        a.setContentPane(gridPanel);
+        JFrame board = new JFrame();
+        board.setTitle("Frog Hopper!");
+        board.setSize(750,750);
+        board.setLayout(new GridLayout(5,5));
+        board.setResizable(false);
 
-        gridPanel.setLayout(new GridLayout(5,5));
-
-        for (int row = 0; row < 25; row++)
+        for(int row = 0; row<5; row++)
         {
-            gridPanel.add(new Square());
+          for(int col = 0; col<5; col++)
+          {
+            if ((row + col) %2 == 0)
+            {
+              Square s = new Square (1, row, col);
+              buttons[row][col] = s;
+            }
+
+            else
+            {
+              Square s = new Square (0, row, col);
+              buttons[row][col] = s;
+
+            }
+            board.add(buttons[row][col]);
+            buttons[row][col].addActionListener(this);
+          }
         }
-        
-        a.setTitle("Frog Hopper!");
-        a.setSize(750,750);
-        a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        a.setVisible(true);
+
+        buttons[1][1].SetImage(2);
+        buttons[1][3].SetImage(2);
+        buttons[2][2].SetImage(2);
+        buttons[4][0].SetImage(2);
+        buttons[4][4].SetImage(2);
+        buttons[4][2].SetImage(3);
+
+
+    
+        board.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        board.setVisible(true);
 
     }
-}
+
+    private Square squareToMove = null;
+
+    public void actionPerformed(ActionEvent e) 
+    {
+
+        Object source = e.getSource();
+
+        for (int r = 0; r < 5; r++)
+        {
+          for (int c = 0; c< 5; c++)
+          {
+
+            if (source == buttons[r][c])
+            {
+              if (squareToMove != null)
+              {
+                squareToMove.MoveTo(buttons[r][c]);
+                buttons[r][c].SetImage(squareToMove.GetType());
+                squareToMove.SetImage(1);
+                squareToMove = null;
+
+                if (buttons[r][c].GetType() == 4)
+                {
+                  buttons[r][c].SetImage(2);
+                }
+                else if (buttons[r][c].GetType() == 5)
+                {
+                  buttons[r][c].SetImage(3);
+                }
+              }
+
+              else if (squareToMove == null)        
+              {
+                if (buttons[r][c].GetType() != 0 || buttons[r][c].GetType() != 1)
+                {
+                  if (buttons[r][c].GetType() == 2)
+                  {
+                    buttons[r][c].SetImage(4);
+                  }
+                  else if (buttons[r][c].GetType() == 3)
+                  {
+                    buttons[r][c].SetImage(5);
+                  }
+                }
+                else 
+                {
+                  squareToMove = null;
+                }
+
+                squareToMove = buttons[r][c];
+              }
+              }
+            }
+          }
+        }
+      }
--- Square.java
+++ Square.java
@@ -4,9 +4,55 @@ import java.applet.Applet;
 
 public class Square extends JButton
 {
-    public Square ()
+    private ImageIcon [] iconArray = new ImageIcon[6];
+
+    private int xCoord;
+    private int yCoord;
+    private int type;
+
+    public Square(int t, int x, int y)
     {
-        ImageIcon x = new ImageIcon("Water.png");
-        this.setIcon(x);
+        this.type = t;
+        this.SetImage(type);
+        this.xCoord = x;
+        this.yCoord = y;
+    }
+
+    public int GetX()
+    {
+        return xCoord;
+    }
+
+    public int GetY()
+    {
+        return yCoord;
+    }
+
+    public int GetType()
+    {
+        return type;
+    }
+
+    public void SetImage (int type)
+    {
+
+        iconArray[0] = new ImageIcon("Water.png", "Water icon");
+        iconArray[1] = new ImageIcon("Lilypad.png", "Lilypad icon");
+        iconArray[2] = new ImageIcon("GreenFrog.png", "GreenFrog icon");
+        iconArray[3]= new ImageIcon("RedFrog.png", "RedFrog icon");
+        iconArray[4] = new ImageIcon("GreenFrog2.png", "GreenFrogClicked icon");
+        iconArray[5] = new ImageIcon("RedFrog2.png", "RedFrogClicked icon");
+
+        this.setIcon(iconArray[type]);
+        
+    }
+
+    public void MoveTo(Square target)
+    {
+        int newX = target.GetX();
+        int newY = target.GetY();
+
+        xCoord = newX;
+        yCoord = newY;
     }
 }
