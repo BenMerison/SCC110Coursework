b27d7eaef3bb37f288e5f6c4bdfb005ac404f054
BenMerison <b.merison@lancaster.ac.uk>
2020-03-19 03:24:35 +0000

    Frogs now must jump over another frog. When another frog has been jumped over, it is now removed

--- Board.java
+++ Board.java
@@ -4,34 +4,29 @@ import java.applet.Applet;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Board implements ActionListener
-{
-    private Square buttons[][] = new Square[5][5];
+public class Board implements ActionListener{
+    private int size = 5;
+    private Square buttons[][] = new Square[size][size];
     private Square squareToMove = null;
     private Object source;
+    private Square squareToRemove;
 
-    public Board ()
-    {
+    public Board (){
         JFrame board = new JFrame();
         board.setTitle("Frog Hopper!");
         board.setSize(750,750);
-        board.setLayout(new GridLayout(5,5));
+        board.setLayout(new GridLayout(size,size));
         board.setResizable(false);
 
-        for(int row = 0; row<5; row++)
-        {
-          for(int col = 0; col<5; col++)
-          {
-            if ((row + col) %2 == 0)
-            {
+        for(int row = 0; row<size; row++){
+          for(int col = 0; col<size; col++){
+            if ((row + col) %2 == 0){
               Square s = new Square (1, row, col);
               buttons[row][col] = s;
             }
-            else
-            {
+            else{
               Square s = new Square (0, row, col);
               buttons[row][col] = s;
-
             }
             board.add(buttons[row][col]);
             buttons[row][col].addActionListener(this);
@@ -48,72 +43,112 @@ public class Board implements ActionListener
         board.setVisible(true);
     }
 
-    public void actionPerformed(ActionEvent e) 
-    {
+    public void actionPerformed(ActionEvent e){
         source = e.getSource();
 
-        for (int r = 0; r < 5; r++)
-        {
-          for (int c = 0; c< 5; c++)
-          {
-            if (source == buttons[r][c])
-            {
+        for (int r = 0; r < 5; r++){
+          for (int c = 0; c< 5; c++){
+            if (source == buttons[r][c]){
               this.processMove(r, c);
             }
           }
         }
     }
 
-    public void processMove (int r, int c)
-    { 
-      if (squareToMove != null && buttons[r][c].getIconType() != 0 && squareToMove != source) 
-      {
-        if (validMove(buttons[r][c].getSquareX(), buttons[r][c].getSquareY(), squareToMove.getSquareX(), squareToMove.getSquareY()))
-        {
+    public void processMove (int r, int c){ 
+      if (squareToMove != null && buttons[r][c].getIconType() != 0 && squareToMove != source){
+        if (validMove(buttons[r][c], squareToMove)){
           buttons[r][c].moveTo(squareToMove);
           squareToMove.changeImage(1);
           squareToMove = null;
           buttons[r][c].buttonClicked();
+          squareToRemove.changeImage(1);
         }
-
-        else{
-          return;
-        }
-    
       }
-      else if (buttons[r][c].getIconType() == 0)
-      {
+      else if (buttons[r][c].getIconType() == 0){
         return;
       }
-      else if (squareToMove == source)
-      {
+      else if (squareToMove == source){
         squareToMove = null;
         buttons[r][c].buttonClicked();
-        return;
       }
-      else 
-      {
-        if (buttons[r][c].getIconType() != 0 && buttons[r][c].getIconType() != 1)
-        {
+      else{
+        if (buttons[r][c].getIconType() != 0 && buttons[r][c].getIconType() != 1){
           buttons[r][c].buttonClicked();
           squareToMove = buttons[r][c];
         }
-        return;
       }
     }
-    public boolean validMove (int rN, int cN, int rO, int cO)
-    {
+    public boolean validMove (Square newSquare, Square oldSquare){
+
+      int rN = newSquare.getSquareX();
+      int rO = oldSquare.getSquareX();
+      int cN = newSquare.getSquareY();
+      int cO = oldSquare.getSquareY();
+      
       int rowChange = Math.abs(rO - rN);
       int colChange = Math.abs(cO - cN);
 
-      if (rowChange == 2 && colChange == 2)
-      {
-        return true;
-      }
-      else if (rowChange == 0 && colChange == 4 || rowChange == 4 && colChange == 0)
-      {
-        return true;
+      squareToRemove = jumpOver(newSquare, oldSquare);
+
+      if (buttons[rN][cN].getIconType() == 1){
+        if (rowChange == 2 && colChange == 2 && squareToRemove.getIconType() == 2){
+          return true;
+        }
+        else if (squareToRemove.getIconType() == 2){
+          if (rowChange == 0 && colChange == 4 || rowChange == 4 && colChange == 0){
+            return true;
+          }
+         return false;
+        }
+        return false;
       }
       return false;
+    }
+
+    public Square jumpOver (Square newSquare, Square oldSquare){
+      int rN = newSquare.getSquareX();
+      int rO = oldSquare.getSquareX();
+      int cN = newSquare.getSquareY();
+      int cO = oldSquare.getSquareY();
+
+      int halfRowChange = (Math.abs(rO - rN))/2;
+      int halfColChange = (Math.abs(cO - cN))/2;
+
+      if (rN > rO && cN == cO){
+        return buttons[rN - halfRowChange][cN];
+      }
+
+      else if (rN < rO && cN == cO){
+        return buttons[rN + halfRowChange][cN];
+      }
+
+      else if (rN == rO && cN > cO){
+        return buttons[rN][cN - halfColChange];
+      }
+
+      else if (rN == rO && cN < cO){
+        return buttons[rN][cN + halfColChange];
+      }
+
+      else if (rN > rO && cN > cO){
+        return buttons[rN - halfRowChange][cN - halfColChange];
+      }
+
+      else if (rN < rO && cN < cO){
+        return buttons[rN + halfRowChange][cN + halfColChange];
+      }
+
+      else if (rN < rO && cN > cO){
+        return buttons[rN + halfRowChange][cN - halfColChange];
+      }
+
+      else if (rN > rO && cN < cO){
+        return buttons[rN - halfRowChange][cN + halfColChange];
+      }
+
+      else{
+        return null;
+      }
     }
 }
